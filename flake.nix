cdd9427eac617ac2e339797a08fd36d1f510feea
erdiloid <test@gmail.com>
2026-05-01 08:23:53 +0000

    rollback the bootstrap to 24.05, enable termux-storage

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "Advanced example of Nix-on-Droid system config with home-manager.";
+
+  inputs = {
+    nixpkgsold.url = "github:NixOS/nixpkgs/nixos-24.05";
+    #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    #nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05"; 
+    home-manager = {
+      #url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
+      
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixcats-nvim-uwu = {
+      url = "github:erdilix/nixcats-nvim-uwu";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-on-droid = {
+      url = "github:nix-community/nix-on-droid/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+  };
+  outputs = { self, nixpkgs, nixpkgsold,  home-manager, nix-on-droid, nixcats-nvim-uwu, ... }@inputs:
+    let
+      system = "aarch64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+	  nix-on-droid.overlays.default
+	  #add other overlays
+        ];
+      };
+    in {
+    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+      modules = [
+        ./nix-on-droid.nix
+	# wrap functionable nix-collect-garbage
+	({pkgs, ...}:{
+	  environment.packages = [
+	    (pkgs.writeShellScriptBin "ngc" ''
+	      exec "${nixpkgsold.legacyPackages.${pkgs.system}.nix}/bin/nix-collect-garbage" "$@" 
+	    '')
+	  ];
+	})
+        # list of extra modules for Nix-on-Droid system
+        # { nix.registry.nixpkgs.flake = nixpkgs; }
+        # ./path/to/module.nix
+
+        # or import source out-of-tree modules like:
+        # flake.nixOnDroidModules.module
+      ];
+
+      # list of extra special args for Nix-on-Droid modules
+      extraSpecialArgs = {
+        # rootPath = ./.;
+    inherit inputs system;
+      };
+    inherit pkgs;
+
+      # set path to home-manager flake 
+      home-manager-path = home-manager.outPath;   
+     };
+      # set nixpkgs instance, it is recommended to apply `nix-on-droid.overlays.default`
+    # standalone home manager confih
+      homeConfigurations = {
+        erdilix = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ 
+            ./home-manager/home.nix
+            nixcats-nvim-uwu.homeModules.default
+          ];
+          extraSpecialArgs = { inherit inputs system; };
+        };
+      };
+    };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,68 @@
+{config, lib, pkgs, ... }:
+
+{
+  # add this 202604
+  home.username = "nix-on-droid"; 
+  home.homeDirectory = "/data/data/com.termux.nix/files/home";
+  # Read the changelog before changing this value
+  home.stateVersion = "24.05";
+  home.packages = with pkgs;[
+	fastfetch
+        htop
+        curl
+	zsh
+	zsh-completions
+	zsh-powerlevel10k
+	ffmpeg
+	tree
+	eza
+	tenere
+        #cmatrix
+	#browsh
+	w3m
+	#alacritty
+	tmux
+	#zellij
+	
+  ];
+  #comment out this part to use stylix
+  #fonts.fontconfig.enable = true;
+  programs.home-manager.enable = true;
+  programs.man.enable = false;
+  nvim.enable = true;
+
+  # insert home-manager config
+  programs.zoxide.enable = true;
+  programs.yazi = {
+	enable = true;
+	
+  };
+  programs.git = {
+	enable = true;
+	userEmail = "test@gmail.com";
+	userName = "erdiloid";
+  };
+  programs.zsh = {
+  	enable = true;
+	enableCompletion = true;
+	autosuggestion.enable = true;
+	syntaxHighlighting.enable = true;
+	#dotDir = ".config/zsh";
+	dotDir = "${config.home.homeDirectory}/.config/zsh";
+	oh-my-zsh = {
+	  enable = true;
+	};
+	initContent = ''
+	  source ${config.home.homeDirectory}/.config/zsh/highlight-colors.zsh
+    	  source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+	  [[ -f ~/.config/zsh/.p10k.zsh ]] && source ~/.config/zsh/.p10k.zsh 
+	'';
+	shellAliases = {
+	  ll = "ls -la";
+	  gs = "git status";
+	};
+    };
+
+ 
+}
+
